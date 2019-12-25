fa9f25d5ef21f11197f7158c97ed7349090de638
Tomas Eriksson <tomeri@pop-os.localdomain>
2019-12-25 15:30:18 +0100

    first commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   private Rigidbody2D rb;
+
+    private Vector2 destination = new Vector2(1.2f, 2);
+
+    public void Set_destination(Vector2 dest) {
+        destination = dest;
+    }
+
+    public Vector2 Get_destination(){
+        return destination;
+    }
+   void Awake(){
+       rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+       rb.bodyType = RigidbodyType2D.Kinematic;
+   }
+
+   void Start(){
+        rb.velocity = destination;
+   }
+}
